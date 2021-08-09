e145931907b24cbd18086e50997ee8cfd9a771ce
feng <fydongmail@163.com>
2021-08-09 20:25:44 +0800

    更新库

---
+++ devices/device.js
@@ -0,0 +1,18 @@
+module.exports = [
+    // Buid-in device
+    'QDPRobot_arduinoUnoUltra',
+    'QDPRobotC02_arduinoEsp32',
+    'QDPRobotIOT_arduinoEsp8266',
+    'arduinoUno',
+    'arduinoNano',
+    'arduinoMini',
+    'arduinoLeonardo',
+    'arduinoMega2560',
+    'arduinoEsp32',
+    'arduinoEsp8266',
+    'microbit',
+    'microbitV2',
+    'makeyMakey'
+    // Third party
+   
+];
---
+++ devices/locales.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'en': {
+        'QDPRobot.description': 'QDP robot.',
+        'QDPRobotC02.description': 'QDP robot.',
+        'QDPRobotIOT.description': 'QDP robot.'
+
+    },
+    'zh-cn': {
+        'QDPRobotC02.description': '齐护机器人C02(ESP32核心)。',
+        'QDPRobot.description': '齐护机器人(ArduinoUNO核心)。',
+        'QDPRobotIOT.description': '物联屏/IOTstart(ESP8266核心)。'
+    }
+};
