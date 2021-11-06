13fa7443c883d7911e1a3b49a372ef26ca6e7093
aopu-edu <postmaster@mircoai.com>
2021-11-06 10:58:26 +0800

    C02实时模式修改QDPRobotC02_arduinoEsp32为QDPRobotC02_arduinoESP32QDP

--- extensions/arduino/kit/QDPRobotC02/index.js
+++ extensions/arduino/kit/QDPRobotC02/index.js
@@ -3,7 +3,7 @@ const QDPRobotC02 = formatMessage => ({
     extensionId: 'QDPRobotC02',
     version: '1.0.0',
     type: 'arduino',
-    supportDevice: ['QDPRobotC02_arduinoEsp32','arduinoEsp32'],
+    supportDevice: ['QDPRobotC02_arduinoESP32QDP','arduinoEsp32'],
     author: 'QDProbot',
     iconURL: `asset/QH.png`,
     description: formatMessage({
